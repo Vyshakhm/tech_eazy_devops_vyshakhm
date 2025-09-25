412542f351536ba9b7bf9428ec699b9d9c747d72
vyshakhm <vyshakhm817@gmail.com>
2025-09-25 22:12:59 +0530

    Implemented S3  Bucket with log collecting for java appliation

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -99,14 +99,6 @@ resource "aws_s3_bucket" "log_bucket" {
   tags = { Name = "log-bucket" }
 }
 
-resource "aws_s3_bucket_public_access_block" "app_logs" {
-  bucket = aws_s3_bucket.app_logs.id
-
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
-}
 
 
 
